54bf9908878c386ae93ca710f91408fa5bd859de
Moritz Küttel <moritz.kuettel@hslu.ch>
2015-09-18 11:25:14 +0200

    implemented Student class

---
+++ containers/src/ch/hslu/prg2/ueb1/Student.java
@@ -0,0 +1,48 @@
+package ch.hslu.prg2.ueb1;
+
+/**
+ *
+ * @author mogria
+ */
+public class Student extends Person {
+    private int studentNumber;
+    private String course;
+    
+    static private int number = 1000;
+
+    public Student(String name, String firstname) {
+        super(name, firstname);
+        studentNumber = number++;
+    }
+    
+    public Student(String name, String firstname, String course) {
+        this(name, firstname);
+        this.course = course;
+    }
+
+    /**
+     * @return the studentNumber
+     */
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    /**
+     * @return the course
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * @param course the course to set
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    
+    @Override
+    public void print() {
+        System.out.println("Student[name = '" + getName() + "', firstname = '" + getFirstname() + "', studentNr = '" + studentNumber + "', course = '" + course + "']");
+    }
+}
